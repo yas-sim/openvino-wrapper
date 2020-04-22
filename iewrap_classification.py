b923725637bf5ae6d582442920f7069a1f6bfaa4
Yasunori Shimura <yasu0710@gmail.com>
2020-04-23 08:20:42 +0900

    simplify classification sample app

--- iewrap_classification.py
+++ iewrap_classification.py
@@ -6,9 +6,9 @@ import numpy as np
 label = open('synset_words.txt').readlines()
 img = cv2.imread('car.png')
 
-ie = iewrap.ieWrapper('public/googlenet-v1/FP16/googlenet-v1.xml', 'CPU', 4)
+ie = iewrap.ieWrapper('public/googlenet-v1/FP16/googlenet-v1.xml', 'CPU')
 
-output = ie.blockInfer(img).reshape((1000,))  # Inferencing
+output = ie.blockInfer(img).reshape((1000,))  # Inference, and reshape the result
 
 # Sort class probabilities and display top 5 classes
 idx = np.argsort(output)[::-1]
